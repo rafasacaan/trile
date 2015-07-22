2af2ed168ef4ff26199d4db481a0a632f626511c
sbstn <sbstn.jmnz@yahoo.com>
2015-07-22 01:21:50 -0300

    TenantsHotFix

--- app/controllers/api/measures_controller.rb
+++ app/controllers/api/measures_controller.rb
@@ -1,6 +1,6 @@
 module API
 	class MeasuresController < ApplicationController
-  		protect_from_forgery with: :null_session
+  	protect_from_forgery with: :null_session
 		before_action :authenticate
 
 		def index
@@ -12,30 +12,35 @@ module API
 			measure = Measure.new(measure_params)
 			if measure.save
 				render json: measure, status: 201, location: api_circuit_measure_path(measure.circuit_id, measure)
-			else 
+			else
 				render json: measure.errors, status: 422
 			end
 		end
-	
+
 		protected
 
 		def authenticate
-        	authenticate_token || render_unauthorized
-    	end
+			authenticate_token || render_unauthorized
+    end
 
-    	def authenticate_token
-        	authenticate_with_http_token do |token, options|
-        		User.find_by(auth_token: token)
-      		end
+    def authenticate_token
+      authenticate_with_http_token do |token, options|
+      	user = User.find_by(auth_token: token)
+		switch_tenant(user.schema_name) unless user.empty?
+		user
     	end
+    end
 
-    	def render_unauthorized(realm=nil)
-    		if realm
-    	        self.headers['WWW-Authenticate'] = %(Token realm="#{realm.gsub(/"/, "")}")
-    	    end
-    	    render json: 'Bad credentials', status: 401
-    	end
+	def switch_tenant(tenant)
+		Apartment::Tenant.switch(tenant)
+	end
 
+    def render_unauthorized(realm=nil)
+    	if realm
+        self.headers['WWW-Authenticate'] = %(Token realm="#{realm.gsub(/"/, "")}")
+      end
+      render json: 'Bad credentials', status: 401
+    end
 
 		def measure_params
 			params.require(:measure).permit(:circuit_id, :watts)
